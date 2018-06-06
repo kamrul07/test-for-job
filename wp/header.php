2427f7d207ac4c4898e6aae11f7ef8d15bb23d87
kamrul0424 <kamrulhasanshuvo04@gmail.com>
2018-06-06 09:02:00 +0600

    first commit

---
+++ wp/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--[if lt IE 9]>
+
+    <![endif]-->
+</head>
+<?php wp_head(); ?>
+<body <?php body_class(); ?>>
---
+++ wp/welcome-page.php
@@ -0,0 +1,172 @@
+<?php
+	/*Template Name: Home */
+	
+	get_header(); ?>
+<!--Header Section -->
+<section class="header-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="header_area">
+					<div class="title_area">
+						<h1 class="header-title text-center">The <br><span>wisdom</span><br> of <br><span>wealth</span></h1>
+					</div>
+					<div class="header_subtitle">
+						<h4 class="header_subtitle text-center">
+							Wealth is the ability to <br>fully experience life. <br> <span>Henry David Thoreau</span>
+						</h4>
+					</div>
+				
+				</div>
+			</div>
+		</div>
+	</div>
+
+</section>
+<!--End header-area -->
+
+<!--Start sub-header-area -->
+<section class="sub-header">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="sub-header_area">
+					<p class="text-center">The secrets of wealth have been a heavily debated topic for millennia. Scholars, philosophers, authors, and economists all have their own opinions on what wealth means to the human experience. Our Wisdom of Wealth booklet shares some of these secrets from over the centuries, with quotes from leading thinkers like Benjamin Franklin, Plato, Thoreau, and others. </p>
+				
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--End sub-header-area -->
+
+<!--Start innersection -area-->
+
+<section class="inner_section innerone">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-6 section_img">
+			
+			</div>
+			<div class="col-md-6 section_content">
+				<div class="quotes">
+					<p class="text-left">Failure seems to be regarded as the one unpardonable crime, success as the all-redeeming virtue, the acquisition of wealth as the single worthy aim of life. <br><span>Henry David Thoreau</span></p>
+				</div>
+			
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<!--End innersection -area-->
+<!--Start innersection -area-->
+
+<section class="inner_section innertwo">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-6 section_content">
+				<div class="quotes">
+					<p class="text-right">The gratification of wealth is not found in mere possession or in lavish expenditure, but in its wise application.<br><span>Miguel de Cervantes</span></p>
+				</div>
+			
+			</div>
+			<div class="col-md-6 section_img">
+			
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<!--End innersection -area-->
+<!--Start innersection -area-->
+
+<section class="inner_section innerthree">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-6 section_img">
+			
+			</div>
+			<div class="col-md-6 section_content">
+				<div class="quotes">
+					<p class="text-left">Gold is a treasure, and he who possesses it does all he wishes to in this world, and succeeds in helping souls into paradise.<br><span>Christopher Columbus</span></p>
+				</div>
+			
+			</div>
+		</div>
+	</div>
+</section>
+<!--End innersection -area-->
+<!--start Contact -area-->
+<section class="contact-area">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h4 class="form-subtitle text-center">Find out what else the wise have said about getting and keeping wealth.</h4>
+				<h2 class="form-title text-center">Get your FREE booklet sent to you today.*</h2>
+				<div class="col-md-6 offset-md-3">
+					<div class="form-area">
+						<form action="">
+							<div class="row">
+								<div class="half_form">
+									<input type="text" class="" placeholder="First Name">
+								</div>
+								<div class="half_form">
+									<input type="text" class="" placeholder="Last Name">
+								</div>
+							
+							
+							</div>
+							<input type="text"  placeholder="Phone Number">
+							<input type="text"  placeholder="Street Address">
+							<input type="text"  placeholder="City">
+							
+							<div class="row"  >
+								<div class="half_form">
+									<select class=" form_padding" >
+										<option value="volvo">State</option>
+										<option value="saab">Dhaka</option>
+										<option value="mercedes">Chattagram</option>
+										<option value="audi">Rajshahi</option>
+									</select>
+								</div>
+								<div class="half_form">
+									<input type="text" class="form_padding" placeholder="Zip Code">
+								</div>
+							</div>
+							<input type="submit" value="Submit">
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--end Contact -area-->
+<!--Start footer quote area-->
+<section class="footer_quote">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<p class="footer_title text-center">Annual income twenty pounds, annual expenditure nineteen [pounds] nineteen [shillings] and six [pence], result happiness. Annual income twenty pounds, annual expenditure twenty pounds ought and six, result misery. <span><br>Charles Dickens</span></p>
+			</div>
+		</div>
+	</div>
+</section>
+<!--End footer quote area-->
+<!--Start Footer area-->
+<section class="footer-area">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<p class="footer-text text-center">*By submitting this request you confirm that Rosland Capital may send you the free information requested and that you are expressly authorizing Rosland Capital to contact you at the telephone number above, irrespective of whether or not such telephone number appears in any state or national Do Not Call registries.</p>
+			</div>
+		</div>
+	</div>
+
+</section>
+
+<!--End footer area-->
+
+<?php get_footer(); ?>
